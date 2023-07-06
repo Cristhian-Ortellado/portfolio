a84d356caba9d53f0b84e244dc39c5ccb9b52298
cristhian <breideworld@gmail.com>
2023-07-06 18:55:52 -0400

    c

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-    loader: 'imgix',
-    path: '/',
-  },
+  // images: {
+  //   loader: 'cloudinary',
+  //   path: '/',
+  // },
 }
 
 module.exports = nextConfig
