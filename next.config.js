ed6ecb915ac8226c8ad25a247009919af2a0d6c3
cristhian <breideworld@gmail.com>
2023-07-06 17:05:30 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+  experimental: {
+    images: {
+        unoptimized: true,
+    },
+},
 }
 
 module.exports = nextConfig
